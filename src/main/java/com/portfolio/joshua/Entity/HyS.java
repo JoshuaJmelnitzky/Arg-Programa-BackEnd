0c1c3d128137da2615cdcc151909a77efbe6173b
Joshua <joshuajmelnitzkysj@gmail.com>
2023-01-07 02:52:00 -0300

    Modificando constraints de entidades

--- src/main/java/com/portfolio/joshua/Entity/HyS.java
+++ src/main/java/com/portfolio/joshua/Entity/HyS.java
@@ -7,6 +7,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 
 @Getter
@@ -21,6 +23,8 @@ public class HyS {
     private String nombre;
 
     @NotNull
+    @Min(1)
+    @Max(100)
     private int porcentaje;
 
     private String img;
